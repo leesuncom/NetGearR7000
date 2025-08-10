37216b2c7a6b5c5fbbc6aa82879612882903cdd5
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-10 22:09:46 +0800

    Update op-part2.sh

--- op-part2.sh
+++ op-part2.sh
@@ -4,44 +4,36 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
+# 确保feeds已安装
+#./scripts/feeds update -a
+#./scripts/feeds install -a
+
+# 定位config_generate文件（根据你的路径确认）
+CONFIG_GENERATE="package/base-files/files/bin/config_generate"
+
+# Modify default IP（已适配原逻辑）
+sed -i 's/192.168.1.1/192.168.3.2/g' "$CONFIG_GENERATE"
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
-sed -i 's/OpenWrt/R7000/g' package/base-files/files/bin/config_generate
+# 1. 修改默认主机名（替换generate_static_system中的hostname）
+sed -i "s/set system.@system\[-1\].hostname='OpenWrt'/set system.@system\[-1\].hostname='R7000'/g" "$CONFIG_GENERATE"
 
-# 替换系统时区和NTP服务器（直接修改配置文件，不依赖uci）
-# 目标文件：package/base-files/files/etc/config/system
-SYSTEM_CONF="package/base-files/files/etc/config/system"
+# 2. 修改默认时区和区域名称
+sed -i "s/set system.@system\[-1\].timezone='GMT0'/set system.@system\[-1\].timezone='CST-8'/g" "$CONFIG_GENERATE"
+sed -i "s/set system.@system\[-1\].zonename='UTC'/set system.@system\[-1\].zonename='Asia\/Shanghai'/g" "$CONFIG_GENERATE"
 
-# 设置时区和区域名称
-sed -i '/option timezone/d' $SYSTEM_CONF  # 删除现有timezone配置
-sed -i '/option zonename/d' $SYSTEM_CONF   # 删除现有zonename配置
-# 在system配置块中添加新设置（定位到config system后插入）
-sed -i '/config system/a\    option timezone '"'CST-8'"'' $SYSTEM_CONF
-sed -i '/config system/a\    option zonename '"'Asia/Shanghai'"'' $SYSTEM_CONF
+# 3. 替换NTP服务器列表（先删除原有server，再添加新服务器）
+# 删除默认的4个openwrt.pool.ntp.org服务器
+sed -i '/add_list system.ntp.server=.openwrt.pool.ntp.org/d' "$CONFIG_GENERATE"
+# 在最后一个add_list位置添加新服务器
+sed -i "/add_list system.ntp.server='3.openwrt.pool.ntp.org'/a\		add_list system.ntp.server='ntp1.aliyun.com'" "$CONFIG_GENERATE"
+sed -i "/add_list system.ntp.server='ntp1.aliyun.com'/a\		add_list system.ntp.server='ntp.tencent.com'" "$CONFIG_GENERATE"
+sed -i "/add_list system.ntp.server='ntp.tencent.com'/a\		add_list system.ntp.server='ntp.ntsc.ac.cn'" "$CONFIG_GENERATE"
+sed -i "/add_list system.ntp.server='ntp.ntsc.ac.cn'/a\		add_list system.ntp.server='time.apple.com'" "$CONFIG_GENERATE"
 
-# 配置NTP服务器
-sed -i '/config ntp/d' $SYSTEM_CONF  # 删除现有ntp配置块
-# 重新添加ntp配置块及服务器列表
-cat >> $SYSTEM_CONF <<EOF
-config ntp
-    option enabled '1'
-    option enable_server '0'
-    list server 'ntp1.aliyun.com'
-    list server 'ntp.tencent.com'
-    list server 'ntp.ntsc.ac.cn'
-    list server 'time.apple.com'
-EOF
 #sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
 #sed -i 's/1.openwrt.pool.ntp.org/ntp.tencent.com/g' package/base-files/files/bin/config_generate
 #sed -i 's/2.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
