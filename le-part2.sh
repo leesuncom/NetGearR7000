fe4a0faa8c8859646f9a50b86285d7d2da73eaa2
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-11 11:06:37 +0800

    Update le-part2.sh

--- le-part2.sh
+++ le-part2.sh
@@ -29,7 +29,7 @@ sed 's/luci-app-wol //g' include/target.mk
 sed 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed 's/OpenWrt/R7000/g' package/base-files/files/bin/config_generate
+sed 's/LEDE/R7000/g' package/base-files/files/bin/config_generate
 
 # 发布固件名称添加日期
 sed 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(shell TZ=UTC-8 date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
