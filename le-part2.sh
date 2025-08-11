fab8500d46b5df0c0820b463af367a4acebd98fa
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-11 11:44:06 +0800

    Update le-part2.sh

--- le-part2.sh
+++ le-part2.sh
@@ -11,25 +11,25 @@
 # See /LICENSE for more information.
 #
 
-sed 's/luci-app-vsftpd//g' include/target.mk
-sed 's/luci-app-filetransfe//g' include/target.mk
-sed 's/luci-app-accesscontrol//g' include/target.mk
-sed 's/luci-app-autoreboot//g' include/target.mk
+sed 's/luci-app-vsftpd //g' include/target.mk
+sed 's/luci-app-filetransfe //g' include/target.mk
+sed 's/luci-app-accesscontrol //g' include/target.mk
+sed 's/luci-app-autoreboot //g' include/target.mk
 sed 's/luci-app-commands//g' include/target.mk
-sed 's/ddns-scripts_dnspod//g' include/target.mk
-sed 's/ddns-scripts_aliyun//g' include/target.mk
-sed 's/luci-app-ddns//g' include/target.mk
-sed 's/block-mount//g' include/target.mk
-sed 's/luci-app-nlbwmon//g' include/target.mk
+sed 's/ddns-scripts_dnspod //g' include/target.mk
+sed 's/ddns-scripts_aliyun //g' include/target.mk
+sed 's/luci-app-ddns //g' include/target.mk
+sed 's/block-mount //g' include/target.mk
+sed 's/luci-app-nlbwmon //g' include/target.mk
 sed 's/luci-app-wol //g' include/target.mk
 
 # Modify default theme
-sed 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname Modify default IP
-sed 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
-sed 's/LEDE/R7000/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/R7000/g' package/base-files/files/bin/config_generate
 
 # 发布固件名称添加日期
-sed 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(shell TZ=UTC-8 date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
+sed -i 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(shell TZ=UTC-8 date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
 
