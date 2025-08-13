85eff6997025601353e884b137e60848370c0386
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-13 17:56:02 +0800

    Update op-part2.sh

--- op-part2.sh
+++ op-part2.sh
@@ -9,40 +9,21 @@
 #./scripts/feeds update -a
 #./scripts/feeds install -a
 
-# 修改默认IP
+
+# Modify default IP（已适配原逻辑）
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 
-# 修改默认主题
+# Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 1. 修改默认主机名
+# 1. 修改默认主机名（替换generate_static_system中的hostname）
 sed -i "s/set system.@system\[-1\].hostname='OpenWrt'/set system.@system\[-1\].hostname='R7000'/g" package/base-files/files/bin/config_generate
 
-# 2. 修改默认时区和区域名称（优化后的版本）
-sed -i "s/'UTC'/'CST-8'/" package/base-files/files/bin/config_generate
-sed -i "/'CST-8'/a\   set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+# 2. 修改默认时区和区域名称
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-# 3. 替换NTP服务器列表（优化后的版本）
-sed -i '/0.openwrt.pool.ntp.org/d' package/base-files/files/bin/config_generate
-sed -i '/1.openwrt.pool.ntp.org/d' package/base-files/files/bin/config_generate
-sed -i '/2.openwrt.pool.ntp.org/d' package/base-files/files/bin/config_generate
-sed -i '/3.openwrt.pool.ntp.org/d' package/base-files/files/bin/config_generate
-sed -i "/system.ntp/d" package/base-files/files/bin/config_generate
-sed -i "/'system'/a\   set system.ntp='ntp1.aliyun.com ntp.tencent.com ntp.ntsc.ac.cn time.apple.com'" package/base-files/files/bin/config_generate
-sed -i "/'system'/a\   set system.ntp_enabled='1'" package/base-files/files/bin/config_generate
-
-
-# 7. 验证修改结果
-echo "=== 验证修改结果 ==="
-echo "IP:"
-grep "192.168.3.2'" package/base-files/files/bin/config_generate
-echo "主机名:"
-grep "hostname='R7000'" package/base-files/files/bin/config_generate
-echo -e "\n时区:"
-grep "timezone='CST-8'" package/base-files/files/bin/config_generate
-grep "zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
-echo -e "\nNTP服务器:"
-grep "ntp='ntp1.aliyun.com" package/base-files/files/bin/config_generate
-
-
-echo "DIY 脚本 part 2 执行完成！"
+# 3. 替换NTP服务器列表（先删除原有server，再添加新服务器）
+sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/time.apple.com/g' package/base-files/files/bin/config_generate
