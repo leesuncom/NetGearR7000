1f1f572bede2f4bbe1a0048402e3ddbd2ebda75a
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-13 22:51:51 +0800

    Update im-part2.sh

--- im-part2.sh
+++ im-part2.sh
@@ -10,6 +10,9 @@
 # See /LICENSE for more information.
 #
 
+# 发布固件名称添加日期
+sed -i 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(shell TZ=UTC-8 date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 
@@ -18,8 +21,3 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 # Modify hostname
 sed -i 's/ImmortalWrt/R7000/g' package/base-files/files/bin/config_generate
-
-# 发布固件名称添加日期
-sed -i 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(shell TZ=UTC-8 date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
-
-
