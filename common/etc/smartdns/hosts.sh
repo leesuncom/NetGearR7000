730709538fdd41bf8d8ae973c0f6fb35921d3f87
leesuncom <70786826+leesuncom@users.noreply.github.com>
2025-08-16 12:05:08 +0800

    Update hosts.sh

--- common/etc/smartdns/hosts.sh
+++ common/etc/smartdns/hosts.sh
@@ -2,6 +2,6 @@
 # 删除
 sudo sed -i '/# ING Hosts Start/,/# ING Hosts End/d' /etc/hosts
 # 添加
-curl -s -k -L https://ghfast.top/https://raw.githubusercontent.com/shidahuilang/hosts/main/hosts | sudo tee -a /etc/hosts
+curl -s -k -L https://ghfast.top/https://raw.githubusercontent.com/shidahuilang/hosts/main/hosts | sudo tee -a common/etc/hosts
 
-/etc/init.d/smartdns restart
+
