d2355bad46eb2a1b7cdee69a35f70d07b534b6e0
leesuncom <1925600159@qq.com>
2025-08-22 17:03:07 +0800

    Update r7000im-part2.sh

--- r7000im-part2.sh
+++ r7000im-part2.sh
@@ -20,13 +20,13 @@ sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generat
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/ImmortalWrt/R7000/g' package/base-files/files/bin/config_generate
-rm -rf package/libs/openssl package/network/services/ppp
-git_clone_path openwrt-24.10 https://github.com/immortalwrt/immortalwrt package/libs/openssl package/network/services/ppp
-sed -i "s/procd-ujail//" include/target.mk
-sed -i "s/tty\(0\|1\)::askfirst/tty\1::respawn/g" target/linux/*/base-files/etc/inittab
+sed -i 's/ImmortalWrt/R7000/g' feeds/package/base-files/files/bin/config_generate
+rm -rf feeds/package/libs/openssl feeds/package/network/services/ppp
+git_clone_path openwrt-24.10 https://github.com/immortalwrt/immortalwrt feeds/package/libs/openssl feeds/package/network/services/ppp
+sed -i "s/procd-ujail//" feeds/include/target.mk
+sed -i "s/tty\(0\|1\)::askfirst/tty\1::respawn/g" feeds/target/linux/*/base-files/etc/inittab
 git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic/hack-6.6
-rm -rf package/system/fstools
+rm -rf feeds/package/system/fstools
 git_clone_path master https://github.com/coolsnowwolf/lede package/system/fstools
 rm -rf target/linux/generic/hack-6.6/767-net-phy-realtek-add-led*
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.6/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.6/
