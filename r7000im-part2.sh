b0063ac07f08da8fbe0893e176339adf07e42a4c
leesuncom <1925600159@qq.com>
2025-08-22 17:54:53 +0800

    Update r7000im-part2.sh

--- r7000im-part2.sh
+++ r7000im-part2.sh
@@ -18,7 +18,7 @@
 sed -i 's/192.168.1.1/192.168.3.2/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名
 sed -i 's/ImmortalWrt/R7000/g' package/base-files/files/bin/config_generate
