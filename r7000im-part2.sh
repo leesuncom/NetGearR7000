889fcb5f03f3739829f8301e482a685f74f43717
leesuncom <1925600159@qq.com>
2025-08-22 16:46:27 +0800

    Update r7000im-part2.sh

--- r7000im-part2.sh
+++ r7000im-part2.sh
@@ -21,7 +21,14 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 # Modify hostname
 sed -i 's/ImmortalWrt/R7000/g' package/base-files/files/bin/config_generate
-
+rm -rf package/libs/openssl package/network/services/ppp
+git_clone_path openwrt-24.10 https://github.com/immortalwrt/immortalwrt package/libs/openssl package/network/services/ppp
 sed -i "s/procd-ujail//" include/target.mk
-
+sed -i "s/tty\(0\|1\)::askfirst/tty\1::respawn/g" target/linux/*/base-files/etc/inittab
+git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic/hack-6.6
+rm -rf package/system/fstools
+git_clone_path master https://github.com/coolsnowwolf/lede package/system/fstools
+rm -rf target/linux/generic/hack-6.6/767-net-phy-realtek-add-led*
+wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.6/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.6/
+rm -rf package/feeds/packages/jool
 # wget -N https://raw.githubusercontent.com/leesuncom/NetGearR7000/refs/heads/main/default-settings/files/99-default-settings-chinese -P feeds/package/emortal/default-settings/files/99-default-settings-chinese
