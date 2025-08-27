da990d005493ad8591241ed9695991b898572a95
leesuncom <1925600159@qq.com>
2025-08-27 10:14:44 +0800

    Update updatew.sh

--- script/updatew.sh
+++ script/updatew.sh
@@ -31,14 +31,18 @@ curl -sS https://anti-ad.net/anti-ad-for-smartdns.conf > r619ac/etc/smartdns/con
 curl -sS https://anti-ad.net/anti-ad-for-smartdns.conf > common/etc/smartdns/conf.d/anti-ad-smartdns.conf
 
 # 获取DNS的SPKI，并按指定格式写入spki文件
-echo "spki_cloudflare: $(echo | openssl s_client -connect '1.0.0.1:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" > r619ac/etc/smartdns/spki
+echo "spki_cloudflare: $(echo | openssl s_client -connect '1.0.0.1:853' -servername cloudflare-dns.com 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" > r619ac/etc/smartdns/spki
 # 获取 8.8.8.8 的 SPKI 并追加到文件（不覆盖原有内容）
-echo "spki_google: $(echo | openssl s_client -connect '8.8.8.8:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> r619ac/etc/smartdns/spki
+echo "spki_google: $(echo | openssl s_client -connect '8.8.8.8:853' -servername dns.google 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> r619ac/etc/smartdns/spki
 # 获取 腾讯 DNSPod 的 SPKI 并追加到文件（不覆盖原有内容）
 echo "spki_DNSPod: $(echo | openssl s_client -connect '120.53.53.53:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> r619ac/etc/smartdns/spki
-echo "spki_cloudflare: $(echo | openssl s_client -connect '1.0.0.1:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" > common/etc/smartdns/spki
+
+
+echo "spki_cloudflare: $(echo | openssl s_client -connect '1.0.0.1:853' -servername cloudflare-dns.com 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" > common/etc/smartdns/spki
 # 获取 8.8.8.8 的 SPKI 并追加到文件（不覆盖原有内容）
-echo "spki_google: $(echo | openssl s_client -connect '8.8.8.8:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> common/etc/smartdns/spki
+echo "spki_8.8.8.8: $(echo | openssl s_client -connect '8.8.8.8:853' -servername dns.google 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> common/etc/smartdns/spki
+echo "spki_google: $(echo | openssl s_client -connect 'https://dns.google/dns-query' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> common/etc/smartdns/spki
+
 # 获取 腾讯 DNSPod 的 SPKI 并追加到文件（不覆盖原有内容）
 echo "spki_DNSPod: $(echo | openssl s_client -connect '120.53.53.53:853' 2> /dev/null | openssl x509 -pubkey -noout | openssl pkey -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64)" >> common/etc/smartdns/spki
 # 获取 SB公共DNS 的 SPKI 并追加到文件（不覆盖原有内容）
