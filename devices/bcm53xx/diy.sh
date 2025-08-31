6d82db204228fb2283791f71c79caad722ddd39a
leesuncom <1925600159@qq.com>
2025-08-31 21:01:44 +0800

    删除

--- devices/bcm53xx/diy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-shopt -s extglob
-
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-
-sed -i "s/^TARGET_DEVICES /# TARGET_DEVICES /" target/linux/bcm53xx/image/Makefile
-sed -i "s/# TARGET_DEVICES += netgear_r7000/TARGET_DEVICES += netgear_r7000/" target/linux/bcm53xx/image/Makefile
-
