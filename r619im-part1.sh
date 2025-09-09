4e157dcfda98b589c605ea5842d4cb56f3572306
leesuncom <1925600159@qq.com>
2025-09-09 13:25:53 +0800

    Update r619im-part1.sh

--- r619im-part1.sh
+++ r619im-part1.sh
@@ -11,12 +11,12 @@
 #
 
 # replace luci-theme-argon to lastest update
-rm -rf feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/packages/theme/luci-theme-argon 
-rm -rf feeds/smpackage/luci-theme-argon 
-rm -rf feeds/smpackage/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/packages/theme/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/packages/theme/luci-theme-argon 
+#rm -rf feeds/smpackage/luci-theme-argon 
+#rm -rf feeds/smpackage/luci-app-argon-config
+#git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/packages/theme/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
 # 移除 openwrt feeds 自带的核心库
 # rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
