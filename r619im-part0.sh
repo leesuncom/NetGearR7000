aa172783018ee8fa6f945c4fa2116f2828f82691
leesuncom <1925600159@qq.com>
2025-09-17 15:36:23 +0800

    Update r619im-part0.sh

--- r619im-part0.sh
+++ r619im-part0.sh
@@ -11,6 +11,7 @@
 #
 
 # Uncomment a feed source
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages.git;main' feeds.conf.default
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
