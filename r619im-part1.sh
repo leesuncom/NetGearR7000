9ee5df90555bb622db73875b9f49d6687697c5e3
leesuncom <1925600159@qq.com>
2025-10-10 21:58:59 +0800

    Update r619im-part1.sh

--- r619im-part1.sh
+++ r619im-part1.sh
@@ -17,7 +17,7 @@
 #rm -rf feeds/smpackage/luci-app-argon-config
 #git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/packages/theme/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
-
+git clone https://github.com/DNSCrypt/dnscrypt-proxy.git feeds/packages/applications/dnscrypt-proxy2
 # 移除 openwrt feeds 自带的核心库
 # rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
@@ -27,13 +27,13 @@
 # git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 
 # replace MOSdns to lastest update
-# rm -rf feeds/smpackage/luci-app-mosdns
-# rm -rf feeds/smpackage/mosdns
-# rm -rf feeds/luci/applications/luci-app-mosdns
-# rm -rf feeds/packages/utils/v2dat
-# rm -rf feeds/packages/net/v2ray-geodata
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+rm -rf feeds/smpackage/luci-app-mosdns
+rm -rf feeds/smpackage/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # replace smartdns to lastest update
 # rm -rf feeds/luci/applications/luci-app-smartdns
