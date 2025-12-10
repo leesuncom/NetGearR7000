76217cb554fd7f35cce5633be4f1980541938cf2
leesuncom <1925600159@qq.com>
2025-12-10 20:43:27 +0800

    20.50

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,14 +23,20 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # 发布固件名称添加日期
 sed -i 's|^IMG_PREFIX\:\=.*|IMG_PREFIX:=K-10.0.0.2-$(shell TZ=UTC-8 date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))|g' include/image.mk
 
+# Modify hostname
+sed -i 's/ImmortalWrt/KR619AC/g' package/base-files/files/bin/config_generate
+
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+
 # Modify default THEME
 #  sed -i 's/luci-theme-bootstrap/luci-theme-argonv3/g' ./feeds/luci/collections/luci/Makefile
+
 # Modify default PASSWORD
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./$1$BtNu28UV$VAZEz4CDe1k7Dvar7Ftji0/g' ./package/lean/default-settings/files/zzz-default-settings
 
 # wget -P ./target/linux/ipq40xx/base-files/etc/hotplug.d/block/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/20-udisk-mount
 # chmod 0755 target/linux/ipq40xx/base-files/etc/hotplug.d/block/20-udisk-mount
+
 sed -i 's/option check_signature 1/option check_signature 0/' package/system/opkg/files/opkg-smime.conf
 sed -i '$a src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/arm_cortex-a7_neon-vfpv4/kiddin9' package/system/opkg/files/customfeeds.conf
