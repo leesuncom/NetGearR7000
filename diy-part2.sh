bf2f0aa29d016070a54f61a9b3c3107aff316fba
leesuncom <1925600159@qq.com>
2025-12-10 09:53:31 +0800

    1210

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 # Modify default PASSWORD
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./$1$BtNu28UV$VAZEz4CDe1k7Dvar7Ftji0/g' ./package/lean/default-settings/files/zzz-default-settings
 
-wget -P ./target/linux/ipq40xx/base-files/etc/hotplug.d/block/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/20-udisk-mount
-chmod 0755 target/linux/ipq40xx/base-files/etc/hotplug.d/block/20-udisk-mount
+# wget -P ./target/linux/ipq40xx/base-files/etc/hotplug.d/block/ https://raw.githubusercontent.com/Mike-qian/dns2tcp/main/20-udisk-mount
+# chmod 0755 target/linux/ipq40xx/base-files/etc/hotplug.d/block/20-udisk-mount
 sed -i 's/option check_signature 1/option check_signature 0/' package/system/opkg/files/opkg-smime.conf
 sed -i '$a src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/arm_cortex-a7_neon-vfpv4/kiddin9' package/system/opkg/files/customfeeds.conf
