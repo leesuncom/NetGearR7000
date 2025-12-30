4a9a202d1cdd73beb6b1b1149fbd72bccbf22849
leesuncom <1925600159@qq.com>
2025-12-30 10:20:51 +0800

    333

--- diyk.sh
+++
@@ -1,6 +0,0 @@
-#sed -i '$a src-git xuanranran https://github.com/xuanranran/openwrt-package.git' feeds.conf.default
-#sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
-# git clone https://github.com/xuanranran/openwrt-package.git feeds/xuanranran
-git clone https://github.com/kiddin9/kwrt-packages.git feeds/kiddin9
-# sed -i '$a src-link xuanranran feeds/xuanranran' feeds.conf.default
-sed -i '$a src-link kiddin9 feeds/kiddin9' feeds.conf.default
